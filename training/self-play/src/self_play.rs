249fdaf5bb0724be66a2f39436f22cfb77d711f9
Barak Ugav <barakugav@gmail.com>
2025-10-05 11:33:50 +0300

    Use crossbeam to send ready, termination, results, self-play tasks

--- training/self-play/src/self_play.rs
+++ training/self-play/src/self_play.rs
@@ -1,17 +1,14 @@
-use itertools::Itertools;
+use crossbeam::channel::{Receiver, Sender};
 use std::fs;
 use std::io::Write;
-use std::ops::Deref;
 use std::path::{self, Path, PathBuf};
-use std::sync::atomic::AtomicUsize;
-use std::sync::{Arc, Mutex};
-use std::thread;
 
 use cattus::game::{GameColor, GameStatus, Position};
 use cattus::mcts::{MctsParams, MctsPlayer};
 use cattus::net;
 
 use crate::serialize::{DataEntry, ToBytes};
+use crate::util::thread::{ThreadControl, ThreadManager};
 
 #[derive(Copy, Clone)]
 pub struct GamesResults {
@@ -55,38 +52,61 @@ where
             }
         }
 
-        let games_counter = Arc::new(AtomicUsize::new(0));
-        let result = Arc::new(Mutex::new(GamesResults { w1: 0, w2: 0, d: 0 }));
+        let (task_sender, task_receiver) = crossbeam::channel::unbounded();
+        let (results_sender, results_receiver) = crossbeam::channel::unbounded();
 
-        let job_builder = || {
-            let worker = SelfPlayWorker::new(
-                self.player1_params.clone(),
-                self.player2_params.clone(),
-                output_dir1.to_path_buf(),
-                output_dir2.to_path_buf(),
-                result.clone(),
-                games_counter.clone(),
-                games_num,
-            );
+        let mut manager = ThreadManager::new();
+        for i in 0..self.thread_num.max(1) {
+            let worker = SelfPlayWorker {
+                player1_params: self.player1_params.clone(),
+                player2_params: self.player2_params.clone(),
+                output_dir1: output_dir1.to_path_buf(),
+                output_dir2: output_dir2.to_path_buf(),
+                task_channel: task_receiver.clone(),
+                results_channel: results_sender.clone(),
+            };
 
-            move || worker.generate_data().unwrap()
-        };
+            manager.spawn_thread(format!("self_play_worker_{}", i), move |control| {
+                control.set_ready();
 
-        /* Spawn thread_num-1 to jobs [1..thread_num-1] */
-        // TODO: add termination mechanism, detect if one of the threads panic
-        let threads = (1..self.thread_num).map(|_| thread::spawn(job_builder())).collect_vec();
-
-        /* Use current thread to do job 0 */
-        job_builder()();
-
-        /* Join all threads */
-        for t in threads {
-            t.join().unwrap();
+                worker.generate_data(control).unwrap();
+            });
         }
+        manager.wait_ready(std::time::Instant::now() + std::time::Duration::from_secs(10));
 
-        let res = *result.lock().unwrap().deref();
-        Ok(res)
+        for game_idx in 0..games_num {
+            task_sender.send(SelfPlayTask { game_idx }).unwrap();
+        }
+        let mut results = Vec::new();
+        while results.len() < games_num {
+            let res = results_receiver.recv_timeout(std::time::Duration::from_secs(1));
+            if manager.any_thread_crashed() {
+                break;
+            }
+            if let Ok(res) = res {
+                results.push(res);
+            }
+        }
+        let join_res = manager.terminate();
+        if let Err(e) = join_res {
+            return Err(std::io::Error::other(format!("Thread panicked: {:?}", e)));
+        }
+        drop(task_sender);
+
+        let mut summery = GamesResults { w1: 0, w2: 0, d: 0 };
+        for res in results {
+            match res {
+                Some(GameColor::Player1) => summery.w1 += 1,
+                Some(GameColor::Player2) => summery.w2 += 1,
+                None => summery.d += 1,
+            }
+        }
+        Ok(summery)
     }
+}
+
+struct SelfPlayTask {
+    game_idx: usize,
 }
 
 struct SelfPlayWorker<Game: cattus::game::Game> {
@@ -94,9 +114,8 @@ struct SelfPlayWorker<Game: cattus::game::Game> {
     player2_params: MctsParams<Game>,
     output_dir1: PathBuf,
     output_dir2: PathBuf,
-    results: Arc<Mutex<GamesResults>>,
-    games_queue: Arc<AtomicUsize>,
-    games_num: usize,
+    task_channel: Receiver<SelfPlayTask>,
+    results_channel: Sender<Option<GameColor>>,
 }
 
 impl<Game> SelfPlayWorker<Game>
@@ -104,42 +123,31 @@ where
     Game: cattus::game::Game,
     DataEntry<Game>: ToBytes,
 {
-    #[allow(clippy::too_many_arguments)]
-    fn new(
-        player1_params: MctsParams<Game>,
-        player2_params: MctsParams<Game>,
-        output_dir1: PathBuf,
-        output_dir2: PathBuf,
-        results: Arc<Mutex<GamesResults>>,
-        games_queue: Arc<AtomicUsize>,
-        games_num: usize,
-    ) -> Self {
-        Self {
-            player1_params,
-            player2_params,
-            output_dir1,
-            output_dir2,
-            results,
-            games_queue,
-            games_num,
-        }
-    }
-
-    fn generate_data(&self) -> std::io::Result<()> {
+    fn generate_data(&self, threads_control: ThreadControl) -> std::io::Result<()> {
         let mut player1 = MctsPlayer::new(self.player1_params.clone());
         let mut player2 = MctsPlayer::new(self.player2_params.clone());
 
         loop {
-            let game_idx = self.games_queue.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
-            if game_idx >= self.games_num {
-                break;
-            }
+            let task = crossbeam::select! {
+                recv(self.task_channel) -> task => {
+                    match task {
+                        Ok(task) => task,
+                        Err(_) => break, // channel closed
+                    }
+                }
+                recv(threads_control.termination_receiver()) -> _ => {
+                    break;
+                }
+            };
 
             let mut game = Game::new();
             let mut pos_probs_pairs = Vec::new();
-            let players_switch = game_idx % 2 == 1;
+            let players_switch = task.game_idx % 2 == 1;
 
             let winner = loop {
+                if threads_control.termination_receiver().try_recv().is_ok() {
+                    return Ok(());
+                }
                 if let GameStatus::Finished(winner) = game.status() {
                     break winner;
                 }
@@ -168,28 +176,15 @@ where
 
             /* Save all data entries */
             for (pos_idx, (pos, probs)) in pos_probs_pairs.into_iter().enumerate() {
-                self.write_data_entry(game_idx, pos_idx, pos, probs, winner)?;
+                self.write_data_entry(task.game_idx, pos_idx, pos, probs, winner)?;
             }
 
             /* Update winning counters */
-            {
-                let mut results = self.results.lock().unwrap();
-                let counter = match winner {
-                    None => &mut results.d,
-                    Some(mut player) => {
-                        if players_switch {
-                            player = player.opposite();
-                        }
-                        match player {
-                            GameColor::Player1 => &mut results.w1,
-                            GameColor::Player2 => &mut results.w2,
-                        }
-                    }
-                };
-                *counter += 1;
-            }
+            self.results_channel
+                .send(winner.map(|c| if players_switch { c.opposite() } else { c }))
+                .unwrap();
 
-            log::debug!("Game {} done", game_idx);
+            log::debug!("Game {} done", task.game_idx);
         }
         Ok(())
     }
@@ -218,7 +213,7 @@ where
         };
 
         let data_entry_bytes = DataEntry { pos, probs, winner }.to_bytes();
-        std::fs::File::create_new(&output_dir.join(format!("{game_idx:#08}_{pos_idx:#03}.traindata",)))?
+        std::fs::File::create_new(output_dir.join(format!("{game_idx:#08}_{pos_idx:#03}.traindata")))?
             .write_all(&data_entry_bytes)
     }
 }
