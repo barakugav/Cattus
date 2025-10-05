0ac2e95069a8bc3d27f7ea5dda0942263c7cf07e
Barak Ugav <barakugav@gmail.com>
2025-10-05 09:39:12 +0300

    Remove dyn DataSerializer, require ToBytes trait for DataEntry<Game>

--- training/self-play/src/bin/hex_self_player.rs
+++ training/self-play/src/bin/hex_self_player.rs
@@ -1,7 +1,6 @@
 use cattus::hex::{HexGame, HEX_STANDARD_BOARD_SIZE};
 use cattus_self_play::self_play_cmd;
-use cattus_self_play::serialize::hex::HexSerializer;
 
 fn main() -> std::io::Result<()> {
-    self_play_cmd::run_main::<HexGame<HEX_STANDARD_BOARD_SIZE>>(Box::new(HexSerializer))
+    self_play_cmd::run_main::<HexGame<HEX_STANDARD_BOARD_SIZE>>()
 }
--- training/self-play/src/serialize/mod.rs
+++ training/self-play/src/serialize/mod.rs
@@ -2,10 +2,42 @@ pub mod chess;
 pub mod hex;
 pub mod ttt;
 
-use std::path::Path;
+use cattus::game::{GameColor, Move};
 
-use crate::self_play::DataEntry;
+pub struct DataEntry<Game: cattus::game::Game> {
+    pub pos: Game::Position,
+    pub probs: Vec<(Game::Move, f32)>,
+    pub winner: Option<GameColor>,
+}
 
-pub trait DataSerializer<Game: cattus::game::Game>: Sync + Send {
-    fn serialize_data_entry(&self, entry: DataEntry<Game>, filename: &Path) -> std::io::Result<()>;
+pub trait ToBytes {
+    fn to_bytes(&self) -> Vec<u8>;
+}
+
+pub fn generic_entry_to_bytes<Game: cattus::game::Game>(
+    planes: &[u64],
+    probs: &[(Game::Move, f32)],
+    winner: i8,
+) -> Vec<u8> {
+    /* Use -1 for illegal moves */
+    let mut probs_vec = vec![-1.0f32; Game::MOVES_NUM];
+
+    /* Fill legal moves probabilities */
+    for (m, prob) in probs {
+        probs_vec[m.to_nn_idx()] = *prob;
+    }
+
+    let u64bytes = u64::BITS as usize / 8;
+    let f32bytes = /* f32::BITS */ 32 / 8;
+    let i8bytes = i8::BITS as usize / 8;
+    let size = planes.len() * u64bytes + probs_vec.len() * f32bytes + i8bytes;
+    let mut bytes = Vec::with_capacity(size);
+
+    /* Serialized in little indian format, should deserialized the same */
+    bytes.extend(planes.into_iter().flat_map(|p| p.to_le_bytes()));
+    bytes.extend(probs_vec.into_iter().flat_map(|p| p.to_le_bytes()));
+    bytes.extend(winner.to_le_bytes());
+    assert_eq!(bytes.len(), size);
+
+    bytes
 }
