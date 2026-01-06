65d02966ac45023c9eb786355293cf544afba6bd
Barak Ugav <barakugav@gmail.com>
2026-01-06 09:50:20 +0200

    Flip hex from rhombus slated right to slated left, more common

--- engine/src/hex/core.rs
+++ engine/src/hex/core.rs
@@ -113,17 +113,17 @@ impl<const BOARD_SIZE: usize> Bitboard for HexBitboard<BOARD_SIZE> {
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 pub struct HexPosition<const BOARD_SIZE: usize> {
     /// The board should be imagined in 2D like so:
-    /// The board is a rhombus, slanted right. So, board[0][BOARD_SIZE - 1] is the "top right end",
-    /// also called the "top end" of the board, and board[BOARD_SIZE - 1][0] is the "bottom end".
-    /// Red tries to move left-right and blue tries to move top-bottom.
+    /// The board is a rhombus, slanted left. So, board[0][0] is the "top left end",
+    /// also called the "top end" of the board, and board[BOARD_SIZE - 1][BOARD_SIZE - 1] is the "bottom end".
+    /// Red tries to connect top-bottom and blue tries to connect left-right.
     pub board_red: HexBitboard<BOARD_SIZE>,
     pub board_blue: HexBitboard<BOARD_SIZE>,
     pub turn: GameColor,
 
-    /* bitmap of all the tiles one can reach from the left side of the board stepping only on tiles with red pieces */
-    left_red_reach: HexBitboard<BOARD_SIZE>,
-    /* bitmap of all the tiles one can reach from the top side of the board stepping only on tiles with blue pieces */
-    top_blue_reach: HexBitboard<BOARD_SIZE>,
+    /* bitmap of all the tiles one can reach from the top side of the board stepping only on tiles with red pieces */
+    top_red_reach: HexBitboard<BOARD_SIZE>,
+    /* bitmap of all the tiles one can reach from the left side of the board stepping only on tiles with blue pieces */
+    left_blue_reach: HexBitboard<BOARD_SIZE>,
     number_of_empty_tiles: u16,
     winner: Option<GameColor>,
 }
@@ -134,8 +134,8 @@ impl<const BOARD_SIZE: usize> HexPosition<BOARD_SIZE> {
             board_red: HexBitboard::new(),
             board_blue: HexBitboard::new(),
             turn: starting_color,
-            left_red_reach: HexBitboard::new(),
-            top_blue_reach: HexBitboard::new(),
+            top_red_reach: HexBitboard::new(),
+            left_blue_reach: HexBitboard::new(),
             number_of_empty_tiles: (BOARD_SIZE * BOARD_SIZE) as u16,
             winner: None,
         }
@@ -162,8 +162,8 @@ impl<const BOARD_SIZE: usize> HexPosition<BOARD_SIZE> {
             board_red,
             board_blue,
             turn,
-            left_red_reach: HexBitboard::new(),
-            top_blue_reach: HexBitboard::new(),
+            top_red_reach: HexBitboard::new(),
+            left_blue_reach: HexBitboard::new(),
             number_of_empty_tiles: (BOARD_SIZE * BOARD_SIZE) as u16
                 - (board_red.get_raw().count_ones() + board_blue.get_raw().count_ones()) as u16,
             winner: None,
@@ -211,11 +211,11 @@ impl<const BOARD_SIZE: usize> HexPosition<BOARD_SIZE> {
     fn neighbors(r: usize, c: usize) -> impl Iterator<Item = (usize, usize)> {
         let connection_dirs = [
             /*      right */ (0, 1),
-            /* up   right */ (-1, 0),
-            /* up   left  */ (-1, -1),
+            /* up   right */ (-1, 1),
+            /* up   left  */ (-1, 0),
             /*      left  */ (0, -1),
-            /* down left  */ (1, 0),
-            /* down right */ (1, 1),
+            /* down left  */ (1, -1),
+            /* down right */ (1, 0),
         ];
         connection_dirs
             .into_iter()
@@ -226,12 +226,12 @@ impl<const BOARD_SIZE: usize> HexPosition<BOARD_SIZE> {
 
     fn update_reach(&mut self, r: usize, c: usize, player: GameColor) {
         let reach_map = match player {
-            GameColor::Player1 => &mut self.left_red_reach,
-            GameColor::Player2 => &mut self.top_blue_reach,
+            GameColor::Player1 => &mut self.top_red_reach,
+            GameColor::Player2 => &mut self.left_blue_reach,
         };
         let is_reach_begin = match player {
-            GameColor::Player1 => |_: usize, c: usize| c == 0,
-            GameColor::Player2 => |r: usize, _: usize| r == 0,
+            GameColor::Player1 => |r: usize, _: usize| r == 0,
+            GameColor::Player2 => |_: usize, c: usize| c == 0,
         };
         if !(is_reach_begin(r, c) || Self::neighbors(r, c).any(|(nr, nc)| reach_map.get(nr * BOARD_SIZE + nc))) {
             return;
@@ -242,20 +242,19 @@ impl<const BOARD_SIZE: usize> HexPosition<BOARD_SIZE> {
             GameColor::Player2 => &self.board_blue,
         };
         let is_reach_end = match player {
-            GameColor::Player1 => |_: usize, c: usize| c == BOARD_SIZE - 1,
-            GameColor::Player2 => |r: usize, _: usize| r == BOARD_SIZE - 1,
+            GameColor::Player1 => |r: usize, _: usize| r == BOARD_SIZE - 1,
+            GameColor::Player2 => |_: usize, c: usize| c == BOARD_SIZE - 1,
         };
 
-        let mut bfs_layer = HexBitboard::<BOARD_SIZE>::new();
+        let mut stack = HexBitboard::<BOARD_SIZE>::new();
         let idx = r * BOARD_SIZE + c;
         reach_map.set(idx, true);
-        bfs_layer.set(idx, true);
+        stack.set(idx, true);
 
-        while !bfs_layer.is_empty() {
-            let idx = bfs_layer.get_raw().trailing_zeros() as usize;
-            bfs_layer.set(idx, false);
-            let r = idx / BOARD_SIZE;
-            let c = idx % BOARD_SIZE;
+        while !stack.is_empty() {
+            let idx = stack.get_raw().trailing_zeros() as usize;
+            stack.set(idx, false);
+            let (r, c) = (idx / BOARD_SIZE, idx % BOARD_SIZE);
 
             if is_reach_end(r, c) {
                 self.winner = Some(player);
@@ -265,7 +264,7 @@ impl<const BOARD_SIZE: usize> HexPosition<BOARD_SIZE> {
                 let n_idx = nr * BOARD_SIZE + nc;
                 if board.get(n_idx) && !reach_map.get(n_idx) {
                     reach_map.set(n_idx, true);
-                    bfs_layer.set(n_idx, true);
+                    stack.set(n_idx, true);
                 }
             }
         }
@@ -299,7 +298,7 @@ impl<const BOARD_SIZE: usize> Position for HexPosition<BOARD_SIZE> {
     fn legal_moves(&self) -> impl Iterator<Item = HexMove<BOARD_SIZE>> {
         (0..(BOARD_SIZE * BOARD_SIZE))
             .filter(|&idx| !self.board_red.get(idx) && !self.board_blue.get(idx))
-            .map(|idx| HexMove::from_idx(idx))
+            .map(HexMove::from_idx)
     }
 
     fn moved_position(&self, m: HexMove<BOARD_SIZE>) -> Self {
@@ -323,8 +322,8 @@ impl<const BOARD_SIZE: usize> Position for HexPosition<BOARD_SIZE> {
             board_red: self.board_blue.flip(),
             board_blue: self.board_red.flip(),
             turn: self.turn.opposite(),
-            left_red_reach: self.top_blue_reach.flip(),
-            top_blue_reach: self.left_red_reach.flip(),
+            top_red_reach: self.left_blue_reach.flip(),
+            left_blue_reach: self.top_red_reach.flip(),
             number_of_empty_tiles: self.number_of_empty_tiles,
             winner: self.winner.map(|w| w.opposite()),
         }
@@ -382,33 +381,33 @@ mod tests {
     #[test]
     fn short_diagonal_wins() {
         let pos: HexStandardPosition = hex_position_from_str(
-            "r . . . . . . . . . .\
-              . r . . . . . . . . .\
-               . . r . . . . . . . .\
-                . . . r . . . . . . .\
-                 . . . . r . . . . . .\
+            ". . . . . . . . . . r\
+              . . . . . . . . . r .\
+               . . . . . . . . r . .\
+                . . . . . . . r . . .\
+                 . . . . . . r . . . .\
                   . . . . . r . . . . .\
-                   . . . . . . r . . . .\
-                    . . . . . . . r . . .\
-                     . . . . . . . . r . .\
-                      . . . . . . . . . r .\
-                       . . . . . . . . . . r\
+                   . . . . r . . . . . .\
+                    . . . r . . . . . . .\
+                     . . r . . . . . . . .\
+                      . r . . . . . . . . .\
+                       r . . . . . . . . . .\
                 b",
         );
         assert_eq!(pos.status(), GameStatus::Finished(Some(GameColor::Player1)));
 
         let pos: HexStandardPosition = hex_position_from_str(
-            "b . . . . . . . . . .\
-              . b . . . . . . . . .\
-               . . b . . . . . . . .\
-                . . . b . . . . . . .\
-                 . . . . b . . . . . .\
+            ". . . . . . . . . . b\
+              . . . . . . . . . b .\
+               . . . . . . . . b . .\
+                . . . . . . . b . . .\
+                 . . . . . . b . . . .\
                   . . . . . b . . . . .\
-                   . . . . . . b . . . .\
-                    . . . . . . . b . . .\
-                     . . . . . . . . b . .\
-                      . . . . . . . . . b .\
-                       . . . . . . . . . . b\
+                   . . . . b . . . . . .\
+                    . . . b . . . . . . .\
+                     . . b . . . . . . . .\
+                      . b . . . . . . . . .\
+                       b . . . . . . . . . .\
             r",
         );
 
@@ -418,45 +417,10 @@ mod tests {
     #[test]
     fn almost_short_diagonal_doesnt_win() {
         let pos: HexStandardPosition = hex_position_from_str(
-            ". . . . . . . . . . .\
-              . r . . . . . . . . .\
-               . . r . . . . . . . .\
-                . . . r . . . . . . .\
-                 . . . . r . . . . . .\
-                  . . . . . r . . . . .\
-                   . . . . . . r . . . .\
-                    . . . . . . . r . . .\
-                     . . . . . . . . r . .\
-                      . . . . . . . . . r .\
-                       . . . . . . . . . . r\
-            b",
-        );
-        assert_eq!(pos.status(), GameStatus::Ongoing);
-
-        let pos: HexStandardPosition = hex_position_from_str(
-            "b . . . . . . . . . .\
-              . b . . . . . . . . .\
-               . . b . . . . . . . .\
-                . . . b . . . . . . .\
-                 . . . . b . . . . . .\
-                  . . . . . b . . . . .\
-                   . . . . . . b . . . .\
-                    . . . . . . . b . . .\
-                     . . . . . . . . b . .\
-                      . . . . . . . . . b .\
-                       . . . . . . . . . . .\
-            r",
-        );
-        assert_eq!(pos.status(), GameStatus::Ongoing);
-    }
-
-    #[test]
-    fn long_diagonal_doesnt_win() {
-        let pos: HexStandardPosition = hex_position_from_str(
             ". . . . . . . . . . r\
               . . . . . . . . . r .\
                . . . . . . . . r . .\
-                . . . . . . . r . . .\
+                . . . . . . . . . . .\
                  . . . . . . r . . . .\
                   . . . . . r . . . . .\
                    . . . . r . . . . . .\
@@ -476,10 +440,45 @@ mod tests {
                  . . . . . . b . . . .\
                   . . . . . b . . . . .\
                    . . . . b . . . . . .\
-                    . . . b . . . . . . .\
+                    . . . . . . . . . . .\
                      . . b . . . . . . . .\
                       . b . . . . . . . . .\
                        b . . . . . . . . . .\
+            r",
+        );
+        assert_eq!(pos.status(), GameStatus::Ongoing);
+    }
+
+    #[test]
+    fn long_diagonal_doesnt_win() {
+        let pos: HexStandardPosition = hex_position_from_str(
+            "r . . . . . . . . . .\
+              . r . . . . . . . . .\
+               . . r . . . . . . . .\
+                . . . r . . . . . . .\
+                 . . . . r . . . . . .\
+                  . . . . . r . . . . .\
+                   . . . . . . r . . . .\
+                    . . . . . . . r . . .\
+                     . . . . . . . . r . .\
+                      . . . . . . . . . r .\
+                       . . . . . . . . . . r\
+            b",
+        );
+        assert_eq!(pos.status(), GameStatus::Ongoing);
+
+        let pos: HexStandardPosition = hex_position_from_str(
+            "b . . . . . . . . . .\
+              . b . . . . . . . . .\
+               . . b . . . . . . . .\
+                . . . b . . . . . . .\
+                 . . . . b . . . . . .\
+                  . . . . . b . . . . .\
+                   . . . . . . b . . . .\
+                    . . . . . . . b . . .\
+                     . . . . . . . . b . .\
+                      . . . . . . . . . b .\
+                       . . . . . . . . . . b\
             r",
         );
         assert_eq!(pos.status(), GameStatus::Ongoing);
